5a91ce991d76f8d31f1fed350be6ce1259b89f26
Miron Marczuk <miron@secret-source.eu>
2022-08-22 14:24:09 +0200

    bundle gem wise_grid

---
+++ spec/wise_grid_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WiseGrid do
+  it "has a version number" do
+    expect(WiseGrid::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
